36a0bb52d1bc03f54887dbb6ae482f171d6b4b42
HiiHongFe <44940083+HiiHongFe@users.noreply.github.com>
2020-01-27 19:42:21 +0800

    Add new icon on homepage
    Add new icon on homepage

--- source/page/menu.ts
+++ source/page/menu.ts
@@ -4,14 +4,30 @@ export default [
         href: ''
     },
     {
+        title: '疫情',
+        href:
+            'https://3g.dxy.cn/newh5/view/pneumonia_peopleapp?from=timeline&isappinstalled=0',
+        icon: 'globe'
+    },
+    {
         title: '医院',
         href: 'hospital',
         icon: 'hospital'
     },
     {
+        title: '酒店',
+        href: '',
+        icon: 'bed'
+    },
+    {
         title: '物流',
         href: 'logistics',
         icon: 'shipping-fast'
+    },
+    {
+        title: '义诊',
+        href: '',
+        icon: 'heartbeat'
     },
     {
         title: '开放源代码',
